bdb62f7f7ea26d9fa75f4fd3b6cbccb74d3c81e4
Jacques Mbabazi <mbabazijacques@gmail.com>
2026-05-28 15:31:27 +0200

    wip: update skill seeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -12,14 +12,14 @@ final class SkillSeeder extends Seeder
     public function run(): void
     {
         $skills = [
-            ['name' => 'Laravel', 'icon' => 'laravel'],
+            ['name' => 'Laravel', 'icon' => 'Laravel'],
             ['name' => 'Git', 'icon' => 'git'],
             ['name' => 'React', 'icon' => 'react'],
             ['name' => 'TypeScript', 'icon' => 'typescript'],
-            ['name' => 'Linux', 'icon' => 'linux'],
+            ['name' => 'Linux', 'icon' => 'Linux'],
             ['name' => 'Tailwind CSS', 'icon' => 'tailwindcss'],
             ['name' => 'Python', 'icon' => 'python'],
-            ['name' => 'Data Analysis', 'icon' => 'dataanalysis'],
+            ['name' => 'Data Analysis', 'icon' => 'data analysis'],
         ];
 
         foreach ($skills as $skill) {
